00eb9ce5e5299a15da8c02c2a6147551a5721a8e
webpackstudent <43551569+webpackstudent@users.noreply.github.com>
2018-10-11 12:34:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+
+возвращает строку rgb вида rgb(3,40, 200)
+
+@param {colour} r - The red component.
+@param {colour} g - The green component.
+@param {colour} b - The blue component.
+
+*/
+function rgb(r = 255, g = 255, b = 255) {
+  return `rgb(${r},${g},${b})`;
+}
